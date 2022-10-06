85ffb332ebaae4e9ce02211926254d02ba92d9e8
A-Der <61888734+A-Der@users.noreply.github.com>
2022-10-06 15:49:59 +0100

    add initial deps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./App.css";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
@@ -18,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
     </div>
   );
 }
