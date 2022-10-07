0e801d6fdeb4b96d6e858aef5affc81857b1bd1b
A-Der <61888734+A-Der@users.noreply.github.com>
2022-10-07 15:20:37 +0100

    render brewery locations on map

--- src/breweryMap/breweryMap.js
+++ src/breweryMap/breweryMap.js
@@ -1,34 +1,53 @@
+import { useState, useEffect } from "react";
 import { useQuery } from "react-query";
-import { Map, Marker } from "pigeon-maps";
+import { Map, Marker, ZoomControl } from "pigeon-maps";
 
 const fetchBreweries = async () => {
   const res = await fetch("https://api.openbrewerydb.org/breweries");
   return res.json();
 };
-
+// onclick marker set centered state
+// open modal of info
+// search certain areas
+// filter
 const BreweryMap = () => {
   const {
     data = [],
     isLoading,
     isError,
   } = useQuery("breweries", fetchBreweries);
+  const [defaultMapCenter, setDefaultMapCenter] = useState([
+    34.802528, -8.567037,
+  ]);
+  const [mapCenter, setMapCenter] = useState([]);
+  const [zoom, setZoom] = useState(4);
 
   return (
     <div className="container">
       <div className="notification is-primary">
         <Map
-          height={300}
-          defaultCenter={[41.289715, -86.627954]}
-          defaultZoom={11}
+          height={400}
+          defaultCenter={defaultMapCenter}
+          center={mapCenter}
+          defaultZoom={2.5}
+          zoom={zoom}
         >
-          {data.map((d) => {
-            return (
-              <Marker
-                width={50}
-                anchor={[parseFloat(d.latitude), parseFloat(d.longitude)]}
-              />
-            );
-          })}
+          <ZoomControl />
+          {data.map(
+            ({ latitude, longitude }, index) =>
+              latitude &&
+              longitude && (
+                <Marker
+                  key={index}
+                  width={50}
+                  anchor={[parseFloat(latitude), parseFloat(longitude)]}
+                  onClick={() => {
+                    setMapCenter([latitude, longitude]);
+                    setZoom(8);
+                  }}
+                />
+              )
+          )}
         </Map>
       </div>
     </div>
